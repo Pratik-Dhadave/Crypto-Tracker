b062d9883d8b4be27140cbda28446520faf49c08
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-12 19:31:22 +0530

    coin & coinCard Component done with name, img, symbol,price

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { server } from '../index';
+import { Container, HStack, Button} from '@chakra-ui/react';
+import Loader from './Loader';
+import ErrorComponent from './ErrorComponent';
+import CoinCard from './CoinCard';
 
 const Coins = () => {
-  return (
-    <div>Coins</div>
-  )
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
+
+  const currencysymbol = 
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
+  
+  const changePage = (page) =>{
+    setPage(page);
+    setLoading(true);
+  }
+
+  const btns = new Array(132).fill(1);
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/markets?vs_currency=${currency}&page=${page}`);
+        setCoins(data);
+        setLoading(false);
+        // console.log(data);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [currency,page])
+
+  if (error) return <ErrorComponent />
+  return <Container maxW={"container.xl"}>
+    {loading ? <Loader /> : <>
+      <HStack wrap={"wrap"}>
+        {
+          coins.map((i) => (
+            <CoinCard
+            id={i.id} 
+            key={i.id}
+            name={i.name} 
+            price={i.current_price}
+            img={i.image} 
+            symbol={i.symbol}
+            currencysymbol={currencysymbol}
+            />
+          ))}
+      </HStack>
+
+      <HStack w={"full"} overflowX={"auto"} p={"8"}>
+        {
+          btns.map((item,index)=>(
+            <Button 
+            bgColor={"blackAlpha.900"} 
+            color={"white"} 
+            onClick={()=>changePage(index+1)}
+            >{index+1}
+            </Button>
+          ))}
+      </HStack>
+    </>}
+  </Container>
 }
 
-export default Coins
+export default Coins;
